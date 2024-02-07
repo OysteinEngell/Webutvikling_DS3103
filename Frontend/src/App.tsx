ae157aed37c8073b8e4e00048bac24817be984b3
OysteinEngell <engell.oystein@gmail.com>
2024-02-07 10:18:23 +0100

    Installed bootstrap

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from 'react-bootstrap'
+import TestComponent from './components/TestComponent';
 
 function App() {
   return (
@@ -8,16 +11,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Eksamen
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant='primary'>Click me!</Button>
+        <TestComponent/>
       </header>
     </div>
   );
---
+++ Frontend/src/components/TestComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button} from 'react-bootstrap'
+
+const TestComponent: React.FC = () => {
+  return (
+    <div>
+      <Button variant='secondary'>Component</Button>
+    </div>
+  )
+}
+
+export default TestComponent
